96acd69c03d10106593a1ee84fce07680407a1eb
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2025-07-23 21:09:42 +0200

    Create Functions.rs

---
+++ Examples/Functions.rs
@@ -0,0 +1,17 @@
+use near_sdk::near;
+
+#[near(contract_state)]
+pub struct Contract{
+    counter: u32
+}
+
+#[near]
+impl Contract{
+    pub fn get_counter(&self, add: u32) -> u32{
+        self.counter + add
+    }
+
+    fn prviate_counter(&self) -> u32{
+        1
+    }
+}
